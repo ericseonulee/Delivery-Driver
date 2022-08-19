45e13a3db1685f72fb4226dbb8cb3f2918e16947
Eric Lee <szl0094@auburn.edu>
2022-08-19 17:50:16 -0500

    package delivery to random customer starts at pick up

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour {
+    private bool hasPackage;
+    private GameObject activeCustomer;
+    private void OnTriggerEnter2D(Collider2D other) {
+        GameObject[] customers = GameObject.FindGameObjectsWithTag("Customer");
+        //Color originalColor = null;
+        if (customers == null) {
+            Debug.LogError("GameObjects with Customer tag not found.");
+        }
+
+        
+
+        if (other.tag == "Package") {
+            Debug.Log("Package picked up.");
+            hasPackage = true;
+
+            int randomNumber = Random.Range(0, customers.Length -1);
+            activeCustomer = customers[randomNumber];
+            activeCustomer.tag = "Active Customer";
+            activeCustomer.GetComponent<SpriteRenderer>().color = Color.red;
+
+            Destroy(other.gameObject);
+        }
+
+        if (activeCustomer && hasPackage && other.tag == "Active Customer") {
+            Debug.Log("Package Delivered!");
+            activeCustomer.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            hasPackage = false;
+        }
+    }
+}
