fb7ae444f54c22cf1c3e9e60bea5a4f64d273a11
Eric Lee <szl0094@auburn.edu>
2022-08-19 18:09:57 -0500

    player changes color when has package

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 originalColor = new Color32(255, 255, 255, 255);
+    SpriteRenderer spriteRenderer;
     private bool hasPackage;
     private GameObject activeCustomer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer == null) {
+            Debug.LogError("SpriteRenderer is null.");
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         GameObject[] customers = GameObject.FindGameObjectsWithTag("Customer");
-        //Color originalColor = null;
         if (customers == null) {
             Debug.LogError("GameObjects with Customer tag not found.");
         }
@@ -19,19 +30,18 @@ public class Delivery : MonoBehaviour {
             hasPackage = true;
 
             int randomNumber = Random.Range(0, customers.Length -1);
-            Debug.Log("customers.Length: " + customers.Length);
-            Debug.Log(randomNumber);
             activeCustomer = customers[randomNumber];
             activeCustomer.tag = "Active Customer";
             activeCustomer.GetComponent<SpriteRenderer>().color = Color.red;
-
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject);
         }
 
         if (activeCustomer && hasPackage && other.tag == "Active Customer") {
             Debug.Log("Package Delivered!");
-            activeCustomer.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            activeCustomer.GetComponent<SpriteRenderer>().color = originalColor;
             activeCustomer.tag = "Customer";
+            spriteRenderer.color = originalColor;
             hasPackage = false;
         }
     }
